e9e32b1a5075f5709d6aabb0f4c582d047c4d405
antonio-source <luccilucci@hotmail.com>
2025-11-01 23:37:49 +0100

    caricamento

---
+++ src/main/java/it/pensioni/calcoloaddizionalecomunale/CalcoloAddizionaleComunaleApplication.java
@@ -0,0 +1,13 @@
+package it.pensioni.calcoloaddizionalecomunale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcoloAddizionaleComunaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcoloAddizionaleComunaleApplication.class, args);
+	}
+
+}
